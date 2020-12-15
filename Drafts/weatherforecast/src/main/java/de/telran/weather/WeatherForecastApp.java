65096848eafecf8b14bbccd0dc02e2f73bc86acc
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-12-15 20:34:42 +0100

    add Logging

--- Drafts/weatherforecast/src/main/java/de/telran/weather/WeatherForecastApp.java
+++ Drafts/weatherforecast/src/main/java/de/telran/weather/WeatherForecastApp.java
@@ -1,9 +1,12 @@
 package de.telran.weather;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import de.telran.weather.service.InputOutputService;
 import de.telran.weather.service.WeatherGateway;
 import de.telran.weather.service.WeatherService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class WeatherForecastApp {
     private InputOutputService inputOutputService;
@@ -19,12 +22,17 @@ public class WeatherForecastApp {
         String result = service.getWeatherByCityName(s);
         inputOutputService.print(result);
     }
+    protected static final Logger log = LogManager.getLogger(WeatherForecastApp.class);
 
     public static void main(String[] args) throws Exception {
-
-        WeatherForecastApp weather = new WeatherForecastApp(new InputOutputService(),
-                new WeatherService(new WeatherGateway(new ObjectMapper())));
-        weather.execute();
-
+        log.info("Started");
+        InputOutputService inputOutputService = new InputOutputService();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        WeatherGateway weatherGateway = new WeatherGateway(mapper);
+        WeatherService service = new WeatherService(weatherGateway);
+        WeatherForecastApp weatherForecastApp = new WeatherForecastApp(inputOutputService, service);
+        weatherForecastApp.execute();
+        log.info("Finished");
     }
 }
